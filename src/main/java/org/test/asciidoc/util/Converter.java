31e8f9f63b6f05a1e77420de0771bf7ce31d8103
luismoramedina <luismoramedina@gmail.com>
2016-02-08 22:19:29 +0100

    asciidoctor java api integration

---
+++ src/main/java/org/test/asciidoc/util/Converter.java
@@ -0,0 +1,35 @@
+package org.test.asciidoc.util;
+
+/**
+ * @author luismoramedina
+ */
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Attributes;
+import org.asciidoctor.Options;
+import org.asciidoctor.SafeMode;
+
+import java.io.File;
+
+public class Converter {
+    public static void main(String[] args) {
+        Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+
+        Attributes attributes = new Attributes();
+        attributes.setBackend("bootstrap");
+        attributes.setAttribute("sourcedir", "c:\\datos\\Snippets\\asciidocs-poc\\src\\main\\");
+        attributes.setAttribute(Attributes.ALLOW_URI_READ, "true");
+
+        Options options = new Options();
+        options.setAttributes(attributes);
+        options.setSafe(SafeMode.UNSAFE);
+
+
+        String s = asciidoctor.convertFile(
+                new File("c:\\datos\\Snippets\\asciidocs-poc\\src\\docs\\asciidoc\\my-test.adoc"),
+                options);
+
+        System.out.println("s = " + s);
+
+    }
+}
